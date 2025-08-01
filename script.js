7ce27fbd666770208ad0187e853fd438a8185a9e
mimibattistella <milena.battistella.ribeiro@escola.pr.gov.br>
2025-08-01 12:19:39 -0300

    fixex

--- script.js
+++ script.js
@@ -50,106 +50,17 @@ const LoginScreen = () => {
   );
 };
 
-const styles= StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-    },
-    
-    header: {
-        backgroundColor: '#ff944d',
-        flexDirection: 'row',
-        alignItems: 'center',
-        padding: 16,
-        },
-    
-        logo: {
-        width: 70,
-        height: 80,
-        borderRadius: 30,
-        backgroundColor: '#fff',
-        borderWidth: 3,
-        borderColor: '#ff944d',
-        justifyContent: 'center',
-        alignItems: 'center',
-        marginRight: 10,
-        },
-    
-        logoText: {
-        color: '#ff944d',
-        fontWeight: 'bold',
-        textAlign: 'center',
-        fontSize: 10,
-        },
-    
-        headerText: {
-        color: '#fff',
-        fontWeight: 'bold',
-        fontSize: 18,
-        },
-    
-        inputContainer: {
-        alignItems: 'center',
-        marginTop: 30,
-        },
-    
-        input: {
-        width: '90%',
-        backgroundColor: '#ff944d',
-        padding: 12,
-        alignItems: 'center',
-        borderRadius: 20,
-        marginBottom: 10,
-        color: '#fff',
-        },
-    
-        forgot: {
-        color: '#ff944d',
-        marginBottom: 20,
-        },
-    
-        googleButton: {
-        flexDirection: 'row',
-        backgroundColor: '#ff944d',
-        padding: 12,
-        borderRadius: 20,
-        alignItems: 'center',
-        },
-    
-        googleIcon: {
-        width: 20,
-        height: 20,
-        marginRight: 10,
-        },
-    
-        googleText: {
-        color: '#fff',
-        fontWeight: 'bold',
-        },
-    
-        footer: {
-        flexDirection: 'row',
-        justifyContent: 'space-around',
-        backgroundColor: '#ff944d',
-        paddingVertical: 10,
-        position: 'absolute',
-        bottom: 0,
-        width: '100%',
-        },
-    
-        footerIcon: {
-        width: 20,
-        height: 20,
-        marginBottom: 5,
-        },
-        
-        footerText: {
-        color: '#fff',
-        fontSize: 12,
-        textAlign: 'center',
-        },
-    
-        
-    
-    
-    })
+
+
+    function navigateTo(page) {
+      if (page === 'home') {
+        // Redireciona para a página Home
+        window.location.href = 'index.html';
+      } else if (page === 'menu') {
+        // Redireciona para a página Menu
+        window.location.href = 'google.com';
+      } else if (page === 'cadastro') {
+        // Redireciona para a página de Cadastro (substitua o caminho conforme necessário)
+        window.location.href = 'file:///C:/Users/aluno/projetomobile-site-receita/pages/cadastro/index.html'; // Caminho fictício
+      }
+    }
